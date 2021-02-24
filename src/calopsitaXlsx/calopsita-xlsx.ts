472990577637f9cb1bb4a722fd729965ca97c74a
Fernando Linhares <fernandolinhares1920@globo.com>
2021-02-24 09:35:59 -0300

    feat: created readme.md

--- src/calopsitaXlsx/calopsita-xlsx.ts
+++ src/calopsitaXlsx/calopsita-xlsx.ts
@@ -1,6 +1,6 @@
 import XlsxPopulate from '../../xlsx/xlsx-populate.min.js'
 
-// Convert Json to Xlsx
+// Convert Json to Xlsx versão
 export class CalopsitaXlsx {
   public convertJsonToXlsx (data: Array<object>, filename: string, password?: string) {
     XlsxPopulate.fromBlankAsync()
