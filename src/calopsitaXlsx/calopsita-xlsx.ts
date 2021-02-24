02c10c5f65369da46c70b724a95b778cce9ce4f6
Fernando Linhares <fernandolinhares1920@globo.com>
2021-02-24 13:00:39 -0300

    refactor: changed promise style

--- src/calopsitaXlsx/calopsita-xlsx.ts
+++ src/calopsitaXlsx/calopsita-xlsx.ts
@@ -2,28 +2,29 @@ import XlsxPopulate from '../../xlsx/xlsx-populate.min.js'
 
 // Convert Json to Xlsx versão
 export class CalopsitaXlsx {
-  public convertJsonToXlsx (data: Array<object>, filename: string, password?: string) {
-    return XlsxPopulate.fromBlankAsync()
-      .then(workbook => {
-        const arrayOfLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-        
-        data.forEach((value, index) => {
-          let arrayNamesValue = Object.keys(value);
-          // Fill header
-          if(index < 26) {
-            workbook.sheet(0).name('Valores').cell(`${arrayOfLetters[index]}${1}`).value(Object.keys(value)[index]);
-          }
+  public async convertJsonToXlsx (data: Array<object>, filename: string, password?: string): Promise<any> {
+    return await new Promise(async (resolve, reject) => {
+      const workbook = await XlsxPopulate.fromBlankAsync();
 
-          for(let i = 1; i <= Object.keys(value).length; i++) {
-            workbook.sheet(0).name('Valores').cell(`${arrayOfLetters[i - 1]}${index + 2}`).value(value[arrayNamesValue[i - 1]]);
-          }
-        })
+      const arrayOfLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
         
-        return workbook.outputAsync({ password: password ? password : '' });
+      data.forEach((value, index) => {
+        let arrayNamesValue = Object.keys(value);
+        // Fill header
+        if(index < 26) {
+          workbook.sheet(0).name('Valores').cell(`${arrayOfLetters[index]}${1}`).value(Object.keys(value)[index]);
+        }
+
+        for(let i = 1; i <= Object.keys(value).length; i++) {
+          workbook.sheet(0).name('Valores').cell(`${arrayOfLetters[i - 1]}${index + 2}`).value(value[arrayNamesValue[i - 1]]);
+        }
       })
-      .then(function (blob) {
+      
+      const blob = await workbook.outputAsync({ password: password ? password : '' });
+
+      try {
         if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-            window.navigator.msSaveOrOpenBlob(blob, `${filename}.xlsx`);
+          window.navigator.msSaveOrOpenBlob(blob, `${filename}.xlsx`);
         } else {
             var url = window.URL.createObjectURL(blob);
             var a = document.createElement("a");
@@ -33,12 +34,13 @@ export class CalopsitaXlsx {
             a.click();
             window.URL.revokeObjectURL(url);
             document.body.removeChild(a);
-            return false;
+            resolve (false);
+          }
+        } catch(err) {
+          alert(err.message || err);
+          reject (err);
         }
-    })
-    .catch(function (err) {
-        alert(err.message || err);
-        throw err;
-    });
+      }
+    )
   }
 }
